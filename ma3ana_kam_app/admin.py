c321052f788bcf987206e428ad220cab9caabf99
Emad Mokhtar <emad.m.habib@gmail.com>
2014-12-31 15:11:01 +0300

    Modify admin search for Period by description

--- ma3ana_kam_app/admin.py
+++ ma3ana_kam_app/admin.py
@@ -4,7 +4,7 @@ from ma3ana_kam_app.models import Expense, Period
 
 class PeriodAdmin(admin.ModelAdmin):
     list_display = ('description', 'start_date', 'end_date', 'amount')
-    search_fields = ('start_date', 'end_date')
+    search_fields = ('start_date', 'end_date', 'description')
 
 
 admin.site.register(Expense)
