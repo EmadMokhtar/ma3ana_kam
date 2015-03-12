ace14a3a592ac83c3e4e6daa2eeaad7048792890
Emad Mokhtar <emad.m.habib@gmail.com>
2015-03-12 11:02:33 +0300

    close #10

--- ma3ana_kam_app/forms.py
+++ ma3ana_kam_app/forms.py
@@ -1,16 +1,19 @@
+import datetime
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from ma3ana_kam_app.models import Expense, Period
 from ma3ana_kam.input_types import DateTypeInput
 
 
 class ExpenseForm(ModelForm):
+    date = forms.DateField(initial=datetime.datetime.now().date(),
+                           widget=DateTypeInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Expense
         fields = ['id', 'date', 'amount', 'description']
         exclude = ['user', 'period']
         widgets = {
-            'date': DateTypeInput(attrs={'class': 'form-control'}),
             'amount': forms.widgets.NumberInput(attrs={'step': 'any', 'class': 'form-control'}),
             'description': forms.widgets.TextInput(attrs={'class': 'form-control'})
         }
