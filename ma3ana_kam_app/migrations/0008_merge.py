67645234e60692624856a9802f70f2ed7c17839c
EmadMokhtar <emad.m.habib@gmail.com>
2015-08-25 18:23:33 +0300

    Merge Migrations

---
+++ ma3ana_kam_app/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ma3ana_kam_app', '0006_auto_20150108_1026'),
+        ('ma3ana_kam_app', '0007_auto_20150823_1358'),
+    ]
+
+    operations = [
+    ]
