2353cb5c355d98779fbbf131cb3b15b027d1d30f
EmadMokhtar <emad.m.habib@gmail.com>
2015-08-29 16:28:28 +0300

    Add docstring and Period List form

--- ma3ana_kam_app/forms.py
+++ ma3ana_kam_app/forms.py
@@ -1,11 +1,14 @@
 import datetime
 from django import forms
 from django.forms import ModelForm, DateInput
-from ma3ana_kam_app.models import Expense, Period
+from .models import Expense, Period, PeriodList
 from ma3ana_kam.input_types import DateTypeInput
 
 
 class ExpenseForm(ModelForm):
+    """
+    Expense form for update and insert
+    """
     date = forms.DateField(initial=datetime.datetime.now().date(),
                            widget=DateTypeInput(attrs={'class': 'form-control'}))
 
@@ -20,6 +23,9 @@ class ExpenseForm(ModelForm):
 
 
 class PeriodForm(ModelForm):
+    """
+    Period from for update and insert
+    """
     class Meta:
         model = Period
         fields = ['start_date', 'end_date', 'amount', 'description']
@@ -30,3 +36,15 @@ class PeriodForm(ModelForm):
             'description': forms.widgets.TextInput(attrs={'class': 'form-control'})
 
         }
+
+class PeriodListForm(ModelForm):
+    """
+    Period List form for update and insert
+    """
+    class Meta:
+        model = PeriodList
+        fields = ['name']
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'form-control'})
+        }
+
