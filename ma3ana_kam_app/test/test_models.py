0a2580cd3cc6bfe08cdef6198c5a415b0cdf86c4
EmadMokhtar <emad.m.habib@gmail.com>
2015-09-05 15:52:10 +0300

    Change import of ValidationError from Django Forms to Django Core.

--- ma3ana_kam_app/test/test_models.py
+++ ma3ana_kam_app/test/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django.forms import ValidationError
+from django.core.exceptions import ValidationError
 from ..models import PeriodList, Period, Expense
 import datetime
 
